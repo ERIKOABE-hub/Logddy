e8a18039dfabe4602488aadc80ea380e7b62a161
Eriko Abe <e.abe1199924@gmail.com>
2026-01-28 11:23:48 +0900

    add: 開発環境のプッシュのためgit追跡ファイルをステージングし、コミット

---
+++ backend/app/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if not DATABASE_URL:
+    raise ValueError('DATABASE_URL environment variable is not set')
+
+print(f'Connecting to database ...')
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping= True,
+    echo= False
+    )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    """
+    データベースセッションを取得
+    使い終わったらセッションを閉じる
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
