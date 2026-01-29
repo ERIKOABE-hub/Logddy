e6e8270daac5b5ef46431cda3e16565d2881521a
Eriko Abe <e.abe1199924@gmail.com>
2026-01-29 17:03:07 +0900

    add: learnerテーブルのモデルを定義

---
+++ backend/app/models/learner.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Text, Datetime, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from app.database import Base
+import uuid
+
+class Learner(Base):
+    __tablenama__ = 'learners'
+
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    firebase_uid = Column(String(128), nullabel=False, unique=True, index=True)
+    name = Column(String(100), nullabel=False)
+    introduction = Column(Text, nullabel=True)
+    role_id = Column(Integer, ForeignKey('roles.id'), nullabel=False, index=True)
+    created_at = Column(Datetime, default=func.now(), nullabel=False)
+    updated_at = Column(Datetime, onupdate=func.now(), nullabel=False)
+    deleted_at = Column(Datetime, nullabel=True, index=True)
+
+    roles = relationship('Role', back_populates='learners')
+    learner_skills = relationship('LearnerSkill', back_populates='learner')
+    logs = relationship('Log', back_populates='learner')
+    likes = relationship('Like', back_populates='learner')
+
+    def __repr__(self):
+        return f'<Learner(id={self.id}, name={self.name})>'
+
