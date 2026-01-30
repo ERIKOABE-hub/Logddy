1a73456574086c77a2d4054e5658af2459793909
Eriko Abe <e.abe1199924@gmail.com>
2026-01-30 11:39:06 +0900

    fix: タイポを修正

--- backend/app/models/skill.py
+++ backend/app/models/skill.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Skill(Base):
-    ___tablename___ = 'skills'
+    __tablename__ = 'skills'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     skill_name = Column(String(100), nullable=False, unique=True)
