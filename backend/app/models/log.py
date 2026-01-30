8d6d0d9542fc08b04c3147780b60563043b90a0d
Eriko Abe <e.abe1199924@gmail.com>
2026-01-30 09:50:16 +0900

    fix: DateTimeとnullableのタイポを修正

--- backend/app/models/log.py
+++ backend/app/models/log.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, Datetime, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database import Base
@@ -8,13 +8,13 @@ class Log(Base):
     __tablename__ = 'logs'
 
     id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    learner_id = Column(String(36), ForeignKey('learners.id'), nullabel=False, index=True)
-    title = Column(String(100), nullabel=False)
-    content = Column(Text, nullabel= False)
-    likes_count = Column(Integer, default=0, nullabel=False)
-    created_at = Column(Datetime, default=func.now(), nullabel=False, index=True)
-    updated_at = Column(Datetime, onupdate=func.now(), nullabel=False)
-    deleted_at = Column(Datetime, nullabel=True, index=True)
+    learner_id = Column(String(36), ForeignKey('learners.id'), nullable=False, index=True)
+    title = Column(String(100), nullable=False)
+    content = Column(Text, nullable= False)
+    likes_count = Column(Integer, default=0, nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False, index=True)
+    updated_at = Column(DateTime, onupdate=func.now(), nullable=False)
+    deleted_at = Column(DateTime, nullable=True, index=True)
 
     learner = relationship('Learners', back_populates='logs')
     likes = relationship('Likes', back_populates='log', cascade='all, delete-orphan')
