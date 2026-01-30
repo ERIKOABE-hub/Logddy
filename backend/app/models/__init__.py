cd599b7bc57f740cabad87435fc61e527f43a2f7
Eriko Abe <e.abe1199924@gmail.com>
2026-01-30 10:07:55 +0900

    add: modelsをモジュール化し、各モデルを集結

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,17 @@
+from app.models.role import Role
+from app.models.learner import Learner
+from app.models.skill import Skill
+from app.models.learner_skill import LearnerSkill
+from app.models.log import Log
+from app.models.like import Like
+from app.models.image import Image
+
+__all__ = [
+    'Role',
+    'Learner',
+    'Skill',
+    'LearnerSkill',
+    'Log',
+    'Like',
+    'Image'
+]
