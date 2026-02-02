d4ff26f339665aacdb268f5217a83ce907ac5bda
Eriko Abe <e.abe1199924@gmail.com>
2026-02-02 19:00:50 +0900

    add: ログイン、新規登録の型を定義

---
+++ frontend/types/auth.ts
@@ -0,0 +1,11 @@
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface SigninCredentials {
+  email: string;
+  password: string;
+}
+
+// todo: AuthResultの型を定義
