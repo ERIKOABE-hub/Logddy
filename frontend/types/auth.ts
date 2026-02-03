4a9159347613da3c0b0f8e206fbc51d42dae8e3b
Eriko Abe <e.abe1199924@gmail.com>
2026-02-03 15:42:22 +0900

    fix: 保存忘れのため再コミット

--- frontend/types/auth.ts
+++ frontend/types/auth.ts
@@ -10,16 +10,6 @@ export interface SigninCredentials {
   password: string;
 }
 
-export interface LoginCredentials {
-  email: string;
-  password: string;
-}
-
-export interface SigninCredentials {
-  email: string;
-  password: string;
-}
-
 export interface AuthReslut {
   user: User;
   token: string;
