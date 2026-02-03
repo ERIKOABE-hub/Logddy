0d898712dee0cbcfc44283b9dc8f4bac52f9e1b5
Eriko Abe <e.abe1199924@gmail.com>
2026-02-03 15:12:48 +0900

    add: ログイン、サインアップの認証結果を型定義

--- frontend/types/auth.ts
+++ frontend/types/auth.ts
@@ -8,4 +8,12 @@ export interface SigninCredentials {
   password: string;
 }
 
-// todo: AuthResultの型を定義
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface SigninCredentials {
+  email: string;
+  password: string;
+}
